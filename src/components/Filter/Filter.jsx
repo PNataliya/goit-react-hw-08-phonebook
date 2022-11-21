0b02a4eae7784d99aa8e1de3806e076daae56808
Nataliya Pogrebniak <pogrebnyak.nataliya@gmail.com>
2022-11-21 05:53:04 +0200

    add change

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,48 +1,7 @@
-// import PropTypes from 'prop-types';
-// import { useDispatch } from 'react-redux';
-// import { setFilter } from '../../redux/contacts/filterSlice';
-
-// import { Label, Text, Input } from './Filter.styled';
-
-// export const Filter = () => {
-//   const dispatch = useDispatch();
-
-//   const handleChange = e => {
-//     e.preventDefault();
-//     const form = e.target;
-//     dispatch(setFilter(form.value.toLowerCase()));
-//   };
-
-//   return (
-//     <Label>
-//       <Text>Input search query</Text>
-//       <Input
-//         type="text"
-//         onChange={handleChange}
-//         name="fiter"
-//         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-//       />
-//     </Label>
-//   );
-// };
-
-// Filter.propTypes = {
-//   handleChange: PropTypes.func,
-//   QueryInputId: PropTypes.func,
-// };
-
-<<<<<<< Updated upstream
-// ==============
-import PropTypes from 'prop-types';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectFilter } from 'redux/contacts/selectors';
-=======
-// ==========
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectFilter } from '../../redux/contacts/selectors';
->>>>>>> Stashed changes
+
 import { setFilter } from '../../redux/contacts/filterSlice';
 
 import { Label, Text, Input } from './Filter.styled';
@@ -52,10 +11,8 @@ export const Filter = () => {
   const filter = useSelector(selectFilter);
 
   const handleChange = e => {
-<<<<<<< Updated upstream
-=======
     e.preventDefault();
->>>>>>> Stashed changes
+
     const { value } = e.target;
     dispatch(setFilter(value));
   };
@@ -68,11 +25,6 @@ export const Filter = () => {
         name="filter"
         value={filter}
         onChange={handleChange}
-<<<<<<< Updated upstream
-=======
-        value={filter}
-        name="fiter"
->>>>>>> Stashed changes
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
       />
